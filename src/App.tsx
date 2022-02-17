f545018145dedcd7cbea988672b07a9d0dfc3d23
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-02-17 22:05:42 +0900

    [Change] #3 ログイン状態とログアウト状態でのコンポーネントを切り分ける

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useEffect, useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import { User } from "./types/user";
+import { getCurrentUser } from "./api/auth";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { SignIn } from "./components/pages/auth/SignIn";
+import { SignUp } from "./components/pages/auth/SignUp";
+import { Home } from "./components/pages/Home";
+
+export const AuthContext = createContext({});
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<User>();
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser();
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.data);
+        console.log(res.data.data);
+      } else {
+        console.log("no current user");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleGetCurrentUser();
+  }, [setCurrentUser]);
+
+  const Private = ({ children }: any) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children;
+      } else {
+        return <Redirect to="/signin" />;
+      }
+    } else {
+      return <></>;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <AuthContext.Provider
+        value={{
+          loading,
+          setLoading,
+          isSignedIn,
+          setIsSignedIn,
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/signin">
+              <SignIn />
+            </Route>
+            <Private>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Private>
+          </Switch>
+        </BrowserRouter>
+      </AuthContext.Provider>
+    </ChakraProvider>
   );
 }
 
