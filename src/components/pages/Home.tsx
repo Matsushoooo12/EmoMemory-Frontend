fa7588ff770b37e5b57495effb2676e9b9d2d26a
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-02-19 22:23:01 +0900

    [Add] #13 新規投稿コンポーネント作成

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,14 @@
-import { Box, Button, Flex, HStack, Image, Textarea } from "@chakra-ui/react";
-import { useState, VFC } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useState, VFC } from "react";
+import "../../App.css";
 
 import HappyFace from "../../images/喜01.png";
 import HappyCard from "../../images/喜付箋.png";
@@ -9,9 +18,12 @@ import SorrowFace from "../../images/哀01.png";
 import SorrowCard from "../../images/哀付箋.png";
 import FunFace from "../../images/楽01.png";
 import FunCard from "../../images/楽付箋.png";
-import "../../App.css";
+import { useHistory } from "react-router-dom";
+import { createPost } from "../../api/post";
 
 export const Home: VFC = () => {
+  const history = useHistory();
+  const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState("happy");
   const handleHappy = () => {
     setEmotion("happy");
@@ -27,6 +39,39 @@ export const Home: VFC = () => {
 
   const handleFun = () => {
     setEmotion("fun");
+  };
+
+  const handleReset = () => {
+    setContent("");
+  };
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleEmotionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmotion(e.target.value);
+  };
+
+  const generateParams = () => {
+    const postParams = {
+      content: content,
+      emotion: emotion,
+    };
+    return postParams;
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const params = generateParams();
+    try {
+      const res = await createPost(params);
+      console.log(res);
+      setContent("");
+      history.push("/");
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <Box pt="80px" width="100%" height="100vh" position="relative">
@@ -85,11 +130,18 @@ export const Home: VFC = () => {
           >
             <Box textAlign="center" pt="150px">
               <form>
+                <Input
+                  type="hidden"
+                  name="emotion"
+                  onChange={handleEmotionChange}
+                />
                 <Textarea
                   variant="unstyled"
                   className="textarea note happy"
                   width="60%"
                   height="210px"
+                  onChange={handleContentChange}
+                  value={content}
                 ></Textarea>
                 <Flex justify="center" mt="56px">
                   <HStack spacing="16px">
@@ -98,6 +150,7 @@ export const Home: VFC = () => {
                       bg="#47789F"
                       color="white"
                       width="80px"
+                      onClick={(e) => handleSubmit(e)}
                     >
                       投稿
                     </Button>
@@ -105,6 +158,7 @@ export const Home: VFC = () => {
                       border="3px solid #47789F"
                       color="#47789F"
                       width="80px"
+                      onClick={handleReset}
                     >
                       リセット
                     </Button>
@@ -125,11 +179,18 @@ export const Home: VFC = () => {
           >
             <Box textAlign="center" pt="150px">
               <form>
+                <Input
+                  type="hidden"
+                  name="emotion"
+                  onChange={handleEmotionChange}
+                />
                 <Textarea
                   variant="unstyled"
                   className="textarea note anger"
                   width="60%"
                   height="210px"
+                  onChange={handleContentChange}
+                  value={content}
                 ></Textarea>
                 <Flex justify="center" mt="56px">
                   <HStack spacing="16px">
@@ -138,6 +199,7 @@ export const Home: VFC = () => {
                       bg="#47789F"
                       color="white"
                       width="80px"
+                      onClick={(e) => handleSubmit(e)}
                     >
                       投稿
                     </Button>
@@ -145,6 +207,7 @@ export const Home: VFC = () => {
                       border="3px solid #47789F"
                       color="#47789F"
                       width="80px"
+                      onClick={handleReset}
                     >
                       リセット
                     </Button>
@@ -165,11 +228,18 @@ export const Home: VFC = () => {
           >
             <Box textAlign="center" pt="150px">
               <form>
+                <Input
+                  type="hidden"
+                  name="emotion"
+                  onChange={handleEmotionChange}
+                />
                 <Textarea
                   variant="unstyled"
                   className="textarea note sorrow"
                   width="60%"
                   height="210px"
+                  onChange={handleContentChange}
+                  value={content}
                 ></Textarea>
                 <Flex justify="center" mt="56px">
                   <HStack spacing="16px">
@@ -178,6 +248,7 @@ export const Home: VFC = () => {
                       bg="#47789F"
                       color="white"
                       width="80px"
+                      onClick={(e) => handleSubmit(e)}
                     >
                       投稿
                     </Button>
@@ -185,6 +256,7 @@ export const Home: VFC = () => {
                       border="3px solid #47789F"
                       color="#47789F"
                       width="80px"
+                      onClick={handleReset}
                     >
                       リセット
                     </Button>
@@ -205,11 +277,18 @@ export const Home: VFC = () => {
           >
             <Box textAlign="center" pt="150px">
               <form>
+                <Input
+                  type="hidden"
+                  name="emotion"
+                  onChange={handleEmotionChange}
+                />
                 <Textarea
                   variant="unstyled"
                   className="textarea note fun"
                   width="60%"
                   height="210px"
+                  onChange={handleContentChange}
+                  value={content}
                 ></Textarea>
                 <Flex justify="center" mt="56px">
                   <HStack spacing="16px">
@@ -218,6 +297,7 @@ export const Home: VFC = () => {
                       bg="#47789F"
                       color="white"
                       width="80px"
+                      onClick={(e) => handleSubmit(e)}
                     >
                       投稿
                     </Button>
@@ -225,6 +305,7 @@ export const Home: VFC = () => {
                       border="3px solid #47789F"
                       color="#47789F"
                       width="80px"
+                      onClick={handleReset}
                     >
                       リセット
                     </Button>
